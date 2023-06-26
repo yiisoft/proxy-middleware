68165afa27910c519931262c80aa4f79d9c7e343
Alexey Rogachev <arogachev90@gmail.com>
2023-06-26 18:23:38 +0600

    Add PHPDoc for exceptions

--- src/Exception/RfcProxyParseException.php
+++ src/Exception/RfcProxyParseException.php
@@ -6,6 +6,12 @@ namespace Yiisoft\ProxyMiddleware\Exception;
 
 use Exception;
 
+/**
+ * Used by {@see TrustedHostsNetworkResolver}. Thrown when parsing of {@see FORWARDED_HEADER_RFC} failed. Forwarded
+ * header is expected to meet the requirements described in RFC 7239.
+ *
+ * @link https://datatracker.ietf.org/doc/html/rfc7239
+ */
 final class RfcProxyParseException extends Exception
 {
 }

eb9234649a718cf8b5873b37d6c24a653a49843e
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-08-06 10:32:42 +0300

    fix

--- src/IpValidator.php
+++ src/IpValidator.php
@@ -17,11 +17,6 @@ final class IpValidator
         return $this->isIpV4($value) || $this->isIpV6($value);
     }
 
-    public function isIpV4(string $value): bool
-    {
-        return preg_match(IpHelper::IPV4_REGEXP, $value) === 1;
-    }
-
     public function isIpV6(string $value): bool
     {
         return preg_match(IpHelper::IPV6_REGEXP, $value) === 1;
@@ -33,5 +28,10 @@ final class IpValidator
     public function inRanges(string $value, array $ranges): bool
     {
         return (new IpRanges($ranges))->isAllowed($value);
+    }
+
+    private function isIpV4(string $value): bool
+    {
+        return preg_match(IpHelper::IPV4_REGEXP, $value) === 1;
     }
 }
